be1c8a69c6a1b57e0a79cbef3eff3b0d433ccd09
shlomit <shlomitshifraw@gmail.com>
2023-05-04 16:12:23 +0300

    multiclipboard app

---
+++ multiclipboard/multiclipboard.py
@@ -0,0 +1,43 @@
+import sys
+import clipboard
+import json
+
+SAVED_DATA = "clipboard.json"
+
+def save_data(file_path, data):
+    with open(file_path, "w") as f:
+        json.dump(data, f)
+        
+        
+def load_data(file_path):
+    try:
+        with open(file_path, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
+    
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    data = load_data(SAVED_DATA)
+    
+    if command == "save":
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA, data)
+        print("data saved!")
+        
+    elif command == "load":
+        key = input("Enter a key: ")
+        if key in data:
+            clipboard.copy(data[key])
+            print("Data copied to clipboard.")
+        else:
+            print("Key does not exist.")
+ 
+    elif command == "list":
+        print(data)
+    else:
+        print("Unknown command")
+else:
+    print("Pleas pass axactly one command ")
